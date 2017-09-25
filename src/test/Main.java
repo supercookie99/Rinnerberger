bb805bd192425d42cb545568920eaed4c46c93da
gruber.n@tele2.at <an2302?99>
2017-09-25 11:01:41 +0200

    doit

---
+++ src/test/Main.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by 5080 on 18.09.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hallo Github!");
+    }
+}
